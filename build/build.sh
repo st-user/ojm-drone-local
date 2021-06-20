f066b07a82c4f8f6d108ef1852f31dd6d1ca3bc2
st-user <nck.cd.339@gmail.com>
2021-06-20 21:37:21 +0900

    Update build script

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,9 @@
 
 source $CONF
 
+MY_GOPATH=`go env GOPATH`
+
+echo $MY_GOPATH
 echo $GOOS
 echo $GOARCH
 
@@ -22,5 +25,21 @@ mkdir work/$TEMP/static
 cp ../.env work/$TEMP/
 cp -R ../client/dist/* work/$TEMP/static/
 mv ../server/tempbinary work/$TEMP/ojm-drone
+
+## LICENSES
+
+cd ../server
+$MY_GOPATH/bin/go-licenses save . --save_path=../build/work/$TEMP/LICENSES/binary
+
+cd ../build
+
+mkdir ./work/$TEMP/LICENSES/static
+mv ./work/$TEMP/static/oss-licenses.json ./work/$TEMP/LICENSES/static/js-licenses.json
+
+BULMA_LCS_URL=https://raw.githubusercontent.com/jgthms/bulma/master/LICENSE
+curl $BULMA_LCS_URL --output work/$TEMP/LICENSES/static/bulma-license.txt
+echo BULMA_LCS_URL > work/$TEMP/LICENSES/static/bulma-license-url.txt
+
+cp README.txt ./work/$TEMP/
 
 env GOOS=$GOOS GOARCH=$GOARCH TEMP=$TEMP node make-directory.js
